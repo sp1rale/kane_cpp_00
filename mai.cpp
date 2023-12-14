1c33cada9954b83253f5173851d25d9f1b6bc280
sp1rale <dimayaremchyk24@gmail.com>
2023-12-14 22:09:37 +0200

    done

--- mai.cpp
+++ mai.cpp
@@ -1,13 +1,40 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;	
 
 int main()
 {
-	std::string name;
-	std::cout << "What is your name? ";
-	std::cin >> name;
-	std::cout << "Hello, " << name << "!\n";
-	return 0;
+    
+    ifstream inputFile("input.txt");
+
+    
+    if (!inputFile.is_open()) {
+        cerr << "Unable to open input file!" << endl;
+        return 1;
+    }
+
+    
+    ofstream outputFile("output.txt");
+
+    
+    if (!outputFile.is_open()) {
+        cerr << "Unable to open output file!" << endl;
+        return 1;
+    }
+
+    string word;
+    while (inputFile >> word) {
+        if (word.length() >= 7) {
+            outputFile << word << " ";
+        }
+    }
+
+    inputFile.close();
+    outputFile.close();
+
+   cout << "Processing complete. Check the 'output.txt' file." << endl;
+
+    return 0;
 }
